c0e737ada526751cd8c4eedc1f770764f3d020cd
tange4315 <36271919+tange4315@users.noreply.github.com>
2019-01-08 12:59:04 -0500

    commented character

--- src/modules/entities/Character.java
+++ src/modules/entities/Character.java
@@ -6,6 +6,10 @@
 package modules.entities;
 
 /**
+ * Creates a Character to be used in a game of Fireboy and Watergirl. A
+ * Character can either be a Fireboy or a Watergirl. Characters can move
+ * throughout each level to attempt to successfully complete it. Characters can
+ * also collect their corresponding Gems as they progress through each Level.
  *
  * @author biGgEsT yEeT: tHe fiNaL fOrM
  */
@@ -15,6 +19,8 @@ public abstract class Character extends Entity {
     private boolean isDead;
 
     /**
+     * Initializes a Character to use in a game of Fireboy and Watergirl using
+     * its x and y position on the screen.
      *
      * @param x an integer representing its x position on the screen
      * @param y an integer representing its y position on the screen
@@ -24,5 +30,4 @@ public abstract class Character extends Entity {
         this.gemsCollected = 0;
         this.isDead = false;
     }
-
 }

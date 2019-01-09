936f858459e5a05ffdab3d63591f53a2e583df65
hadim9637 <hadim9637@HRH-IC0027334.wrdsb.ca>
2019-01-09 08:06:03 -0500

    Started/maybe finished? Watergirl, Fireboy, Character, Platform.

---
+++ src/modules/entities/MovingPlatform.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modules.entities;
+
+/**
+ *
+ * @author hadim9637
+ */
+public class MovingPlatform extends Platform {
+
+    /**
+     *
+     * @param x an integer representing its x position on the screen
+     * @param y an integer representing its y position on the screen
+     * @param width an integer representing the width of the platform
+     * @param height an integer representing the height of the platform
+     */
+    public MovingPlatform(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+}

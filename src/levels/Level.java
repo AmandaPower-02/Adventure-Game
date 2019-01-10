89ad9535967569856ed4e23be1d5f50debbebd74
tange4315 <36271919+tange4315@users.noreply.github.com>
2019-01-10 12:41:44 -0500

    creating the first level

--- src/levels/Level.java
+++ src/levels/Level.java
@@ -5,10 +5,70 @@
  */
 package levels;
 
+import modules.entities.Button;
+import modules.entities.Fire;
+import modules.entities.FireDoor;
+import modules.entities.FireGem;
+import modules.entities.Fireboy;
+import modules.entities.MovingPlatform;
+import modules.entities.Mud;
+import modules.entities.Platform;
+import modules.entities.Water;
+import modules.entities.WaterDoor;
+import modules.entities.WaterGem;
+import modules.entities.Watergirl;
+
 /**
- *
+ * Creates new Levels for Fireboy and Watergirl.
+ * 
  * @author biGgEsT yEeT: tHe fiNaL fOrM
  */
-public class Level {
-    
+public abstract class Level {
+
+    Fireboy fireboy;
+    Watergirl watergirl;
+    Platform[] platforms;
+    MovingPlatform[] movingPlatforms;
+    Fire[] fire;
+    Water[] water;
+    Mud[] mud;
+    Button[] buttons;
+    FireGem[] fireGems;
+    WaterGem[] waterGems;
+    FireDoor fireDoor;
+    WaterDoor waterDoor;
+    private boolean levelComplete;
+
+    /**
+     * Initializes a Level of a game of Fireboy and Watergirl using various
+     * entities including: Characters, Platforms, Obstacles, Gems, and Doors.
+     *
+     * @param f a Character representing a Fireboy
+     * @param w a Character representing a Watergirl
+     * @param p an array of Platforms
+     * @param m an array of Platforms representing MovingPlatforms
+     * @param fo an array of Obstacles representing Fire
+     * @param wo an array of Obstacles representing Water
+     * @param mo an array of Obstacles representing Mud
+     * @param b an array of Obstacles representing Buttons
+     * @param fg an array of Gems representing FireGems
+     * @param wg an array of Gems representing WaterGems
+     * @param fd a Door representing a FireDoor
+     * @param wd a Door representing a WaterDoor
+     */
+    public Level(Fireboy f, Watergirl w, Platform[] p, MovingPlatform[] m, Fire[] fo, Water[] wo, Mud[] mo, Button[] b, FireGem[] fg, WaterGem[] wg, FireDoor fd, WaterDoor wd) {
+        this.fireboy = f;
+        this.watergirl = w;
+        this.platforms = p;
+        this.movingPlatforms = m;
+        this.fire = fo;
+        this.water = wo;
+        this.mud = mo;
+        this.buttons = b;
+        this.fireGems = fg;
+        this.waterGems = wg;
+        this.fireDoor = fd;
+        this.waterDoor = wd;
+        this.levelComplete = false;
+    }
 }
--- src/modules/components/Component.java
+++ src/modules/components/Component.java
@@ -12,20 +12,19 @@ import modules.entities.Entity;
  * @author Amanda
  */
 public abstract class Component {
-    
+
     protected String type;
-    
-    
-    public Component (String type){
+
+    public Component(String type) {
         this.type = type;
     }
-    
-    public String getType(){
+
+    public String getType() {
         return type;
     }
-    
-    public abstract void initialize (Entity entity);
+
+    public abstract void initialize(Entity entity);
+
     public abstract void update(Entity entity);
-    
-    
+
 }

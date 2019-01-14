5c260b7c46ace2e7d754bb86ad0dac13f6a34a88
hadim9637 <hadim9637@HRH-IC0027334.wrdsb.ca>
2019-01-14 08:06:22 -0500

    Added level 2 & 3 classes

--- src/levels/LevelTwo.java
+++ src/levels/LevelTwo.java
@@ -5,10 +5,34 @@
  */
 package levels;
 
+import modules.entities.Fire;
+import modules.entities.Fireboy;
+import modules.entities.Mud;
+import modules.entities.Water;
+import modules.entities.WaterGem;
+import modules.entities.Watergirl;
+
 /**
  *
- * @author hadim9637
+ * @author biGgEsT yEeT: tHe fiNaL fOrM
  */
-public class LevelTwo {
-    
+public class LevelTwo extends Level {
+
+    public LevelTwo() {
+        super.fireboy = new Fireboy(450, 80);
+        super.watergirl = new Watergirl(360, 80);
+
+        super.platforms = new Platform[];
+
+        super.mud = new Mud[1];
+        super.mud[0] = new Mud(20, 620, 840, 20);
+
+        super.fire = new Fire[1];
+
+        super.water = new Water[1];
+
+        super.waterGems = new WaterGem[6];
+
+    }
+
 }
